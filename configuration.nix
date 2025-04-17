4763b6e8ec4a3ca371cf5fc196ddf51c5f865b27
HiboMan <142530154+HiboMan@users.noreply.github.com>
2025-04-17 03:30:14 +0200

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -134,12 +134,6 @@
   nixpkgs.config.allowUnfree = true;
 
   environment = {
-    variables = {
-      #LIBVA_DRIVER_NAME="nvidia";
-      #XDG_SESSION_TYPE = "wayland";
-      #GBM_BACKEND = "nvidia-drm";
-      #__GLX_VENDOR_LIBRARY_NAME = "nvidia";
-    };
   };
 
   fonts.packages = with pkgs; [

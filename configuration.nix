bdd4128026eb106e83e836a910cd417f2561cac7
HiboMan <142530154+HiboMan@users.noreply.github.com>
2025-04-30 16:07:45 +0200

    NixHelper via Nix Programs

--- configuration.nix
+++ configuration.nix
@@ -126,7 +126,6 @@
       wine
       heroic
       fuse
-      nh
     ];
   };
 
@@ -134,6 +133,7 @@
     firefox.enable = true;
     git.enable = true;
     nix-ld.enable = true;
+    nh.enable = true;
     xwayland.enable = true; # probably deprecated
     steam = {
       enable = true;
